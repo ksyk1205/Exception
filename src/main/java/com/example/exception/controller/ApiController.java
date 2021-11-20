f810a7aaee7f8e01c47b7ec54dbcdb43dceb64a6
Gwak seyeon <ksyk1205@naver.com>
2021-11-20 16:37:25 +0900

    SpringBoot Validation 모범사례

--- src/main/java/com/example/exception/controller/ApiController.java
+++ src/main/java/com/example/exception/controller/ApiController.java
@@ -4,16 +4,26 @@ import com.example.exception.dto.User;
 import org.apache.coyote.Response;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.*;
 
 import javax.validation.Valid;
+import javax.validation.constraints.Min;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
 
 @RestController
 @RequestMapping("/api/user")
+@Validated
 public class ApiController {
     @GetMapping("")
-    public User get( @RequestParam(required = false) String name, @RequestParam(required = false) Integer age){
+    public User get(
+            @Size(min = 2)
+            @RequestParam String name,
+            @NotNull
+            @Min(1)
+            @RequestParam Integer age){
         User user = new User();
         user.setName(name);
         user.setAge(age);
